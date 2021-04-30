447db09e611a74e665d14f79939b3c8771ef5e9a
slsay <sl_yang@dreamarts.co.jp>
2021-04-30 16:52:12 +0800

    update: blog type style

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,8 +2,8 @@ exports.BLOG_PAGE_LIMIT = 10;
 exports.LEET_CODE_PAGE_LIMIT = 20;
 
 exports.TypeColor = {
-  "学习整理": "#faad14",
+  "学习整理": "#d4b106",
   "问题解决": "#52c41a",
-  "BUG复盘": "#ffa39e",
+  "BUG复盘": "#ff4d4f",
   "其他": "#b3adad"
 };
